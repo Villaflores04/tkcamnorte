175fd54fb7e4e0339e526bed525dc082c4bb5005
Villaflores04 <tkpaulvincent042005@gmail.com>
2026-09-08 22:15:49 +0800

    chore: add globe configuration

---
+++ public/js/three-globe-config.js
@@ -0,0 +1,6 @@
+/* TK Cam Norte 3D experience configuration. */
+export const TK_GLOBE_CONFIG={
+  colors:{navy:'#071A3D',blue:'#0D47C9',blueGlow:'#3F7BF0',yellow:'#FFC629',white:'#FFFFFF'},
+  idleRotationSpeed:0.00035,
+  markerPulseMs:1800
+};
